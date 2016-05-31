d130545b56c74fb55446ba9019bbf460bfc6fd6a
sameer kumar <kumarsameer1992@gmail.com>
2016-05-31 13:21:06 -0500

    Tests for Scope Updated

--- test/weapon/TestScope.java
+++ test/weapon/TestScope.java
@@ -10,11 +10,38 @@ public class TestScope
 {
 
 	@Test
-	public void test() throws AttachmentException
+	public void testOneAttachment() throws AttachmentException
 	{
 		Weapon weapon = new Pistol();
 		Scope scope = new Scope(weapon);
 		assertEquals(12, scope.fire(10));
 	}
 
+	@Test
+	public void testMltipleAttachment() throws AttachmentException
+	{
+		Weapon weapon = new Pistol();
+		Stabilizer stabilizer = new Stabilizer(weapon);
+		Scope scope = new Scope(stabilizer);
+
+		assertEquals(16, scope.fire(10));
+		assertEquals(0, scope.fire(30));
+
+		weapon = new Pistol();
+		PowerBooster booster = new PowerBooster(weapon);
+		scope = new Scope(booster);
+
+		assertEquals(24, scope.fire(10));
+		assertEquals(0, scope.fire(30));
+
+	}
+
+	@Test(expected = AttachmentException.class)
+	public void testMaxAttachments() throws AttachmentException
+	{
+		Weapon weapon = new PlasmaCannon();
+		PowerBooster booster = new PowerBooster(weapon);
+		Stabilizer stabilizer = new Stabilizer(booster);
+		Scope scope = new Scope(stabilizer);
+	}
 }

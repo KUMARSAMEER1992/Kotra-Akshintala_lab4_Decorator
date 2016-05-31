23a08450cf37c24ffbe635a942175b93741b54e0
sameer kumar <kumarsameer1992@gmail.com>
2016-05-31 12:28:16 -0500

    Pistol updated

--- src/weapon/Pistol.java
+++ src/weapon/Pistol.java
@@ -3,14 +3,15 @@ package weapon;
 public class Pistol extends GenericWeapon
 {
 
-	private static final int PISTOL_BASE_DAMAGE = 10;
-	private static final int PISTOL_MAX_RANGE = 25;
-	private static final int PISTOL_RATE_OF_FIRE = 2;
-	private static final int PISTOL_MAX_AMMO = 10;
-
 	public Pistol()
 	{
-		super(PISTOL_BASE_DAMAGE, PISTOL_MAX_RANGE, PISTOL_RATE_OF_FIRE, PISTOL_MAX_AMMO);
+		/*
+		 * PISTOL_BASE_DAMAGE = 10;
+		 * PISTOL_MAX_RANGE = 25;
+		 * PISTOL_RATE_OF_FIRE = 2;
+		 * PISTOL_MAX_AMMO = 10;
+		 */
+		super(10, 25, 2, 10);
 	}
 
 	@Override
--- test/weapon/TestPistol.java
+++ test/weapon/TestPistol.java
@@ -8,10 +8,21 @@ public class TestPistol
 {
 
 	@Test
-	public void test()
+	public void testInitilization()
+	{
+		Pistol pistol = new Pistol();
+		assertEquals(10, pistol.getBaseDamage());
+		assertEquals(25, pistol.getMaxRange());
+		assertEquals(2, pistol.getRateOfFire());
+		assertEquals(10, pistol.getMaxAmmo());
+	}
+
+	@Test
+	public void testDamage()
 	{
 		Pistol pistol = new Pistol();
 		assertEquals(8, pistol.fire(10));
+		assertEquals(4, pistol.fire(20));
+		assertEquals(0, pistol.fire(30));
 	}
-
 }

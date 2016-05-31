926f3aa1dc07dec6b2d3ca9c5177c5728a19b583
AdiPrathyu3117 <prathyusha.akshintala17@gmail.com>
2016-05-31 13:46:48 -0400

    Tests organized for Stabilizer

--- test/weapon/TestStabilizer.java
+++ test/weapon/TestStabilizer.java
@@ -20,22 +20,16 @@ public class TestStabilizer
 	@Test
 	public void testMltipleAttachment() throws AttachmentException
 	{
-		Weapon weapon = new PlasmaCannon();
-		PowerBooster booster=new PowerBooster(weapon);
-		Stabilizer stabilizer = new Stabilizer(booster);
-		
-		assertEquals(108, stabilizer.fire(15));
-		weapon.updateTime(1);
-		assertEquals(0, stabilizer.fire(35));
+		Weapon weapon;
 		
 		weapon = new PlasmaCannon();
 		Scope scope=new Scope(weapon);
-		stabilizer = new Stabilizer(scope);
+		Stabilizer stabilizer = new Stabilizer(scope);
 
 		assertEquals(77, stabilizer.fire(15));
 		weapon.updateTime(1);
 		assertEquals(0, stabilizer.fire(35));
-		
+
 		weapon = new PlasmaCannon();
 		stabilizer = new Stabilizer(weapon);
 		Stabilizer stabilizer2=new Stabilizer(stabilizer);
@@ -43,6 +37,15 @@ public class TestStabilizer
 		assertEquals(77, stabilizer2.fire(15));
 		weapon.updateTime(1);
 		assertEquals(0, stabilizer2.fire(35));
+		
+		weapon = new PlasmaCannon();
+		PowerBooster booster=new PowerBooster(weapon);
+		stabilizer = new Stabilizer(booster);
+		
+		assertEquals(108, stabilizer.fire(15));
+		weapon.updateTime(1);
+		assertEquals(0, stabilizer.fire(35));
+		
 	}
 	
 	@Test(expected = AttachmentException.class)

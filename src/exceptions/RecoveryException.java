c89cc295f0613fbe32e9a061b70ee4edc3fbe917
sameer kumar <kumarsameer1992@gmail.com>
2016-05-31 19:15:38 -0500

    Updated comments to Exception classes

--- src/exceptions/RecoveryException.java
+++ src/exceptions/RecoveryException.java
@@ -1,7 +1,16 @@
 package exceptions;
 
+/**
+ * Recovery Exception is thrown when the Alien is created with negative recovery
+ * rate.
+ * @author Sameer Kumar Kotra
+ */
 public class RecoveryException extends Exception
 {
+	/**
+	 * Creates RecoveryException with given Message
+	 * @param message
+	 */
 	public RecoveryException(String message)
 	{
 		super(message);
